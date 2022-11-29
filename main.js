0e2429156cecf335819785b489c8d9d76e90af45
Vladakirichenko <vladakirichenko3@gmail.com>
2022-11-29 23:11:23 +0300

    first home work

---
+++ main.js
@@ -0,0 +1,20 @@
+// Task 1
+
+const nameUser = prompt('Your name?');
+alert(`Hello, ${nameUser}! How are you?`)
+
+// Task 2
+
+const num1 = parseInt(prompt('Input num 1'));
+const num2 = parseInt(prompt('Input num 2'));
+
+alert(`${nameUser} entered numbers ${num1} and ${num2}`);
+alert(`${num1 + num2}\n${num1 - num2}\n${num1 * num2}\n${num1 / num2}`);
+
+// Task 3 
+
+const n1 = parseInt(prompt('Input num 1'));
+const n2 = parseInt(prompt('Input num 2'));
+const n3 = parseInt(prompt('Input num 3'));
+
+alert((n1 + n2 + n3) / 3)

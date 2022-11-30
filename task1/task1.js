2b32bc10c9d948c53875d882cf7657cf49aa5cf7
Vladakirichenko <vladakirichenko3@gmail.com>
2022-11-30 10:23:42 +0300

    Home work

---
+++ task1/task1.js
@@ -0,0 +1,7 @@
+// Task 1
+
+const nameUser = prompt('Your name?');
+alert(`Hello, ${nameUser}! How are you?`)
+
+
+
---
+++ task3/task3.js
@@ -0,0 +1,7 @@
+// Task 3 
+
+const n1 = parseInt(prompt('Input num 1'));
+const n2 = parseInt(prompt('Input num 2'));
+const n3 = parseInt(prompt('Input num 3'));
+
+alert((n1 + n2 + n3) / 3)
